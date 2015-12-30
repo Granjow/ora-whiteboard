c176c0c0cd6cabb555a2df0669db8ead3b9b2ca5
Simon A. Eugster <simon.eugster@eps.ch>
2015-12-30 08:53:49 +0100

    Test: Convert to PNG

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var fs = require( 'fs' ),
+    path = require( 'path' ),
     oti = require( 'ora-to-image' );
 
-fs.watch( 'board.ora', function ( event, filename ) {
+var oraPath = path.join( __dirname, 'board.ora' );
+
+fs.watch( oraPath, function ( event, filename ) {
     if ( event === 'change' ) {
         console.log( 'File changed:', filename );
+        oti.oraToImage( oraPath, 'out.png' );
     } else {
         console.warn( 'Unknown event: ', event );
     }
