fba85ecc62c48f5440b2baceff794d49f6b799bc
Simon A. Eugster <simon.eugster@eps.ch>
2015-12-30 08:51:13 +0100

    Vagrant configuration

---
+++ Vagrant/saltstack/pillar/node.sls
@@ -0,0 +1,5 @@
+node:
+  version: 5.3.0
+  checksum: cc05ff06149c638345835788f448471d264a7e011bf083394f86d5be51975c7e
+  make_jobs: 2
+  install_from_source: True
--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var fs = require( 'fs' ),
     oti = require( 'ora-to-image' );
+
+fs.watch( 'board.ora', function ( event, filename ) {
+    if ( event === 'change' ) {
+        console.log( 'File changed:', filename );
+    } else {
+        console.warn( 'Unknown event: ', event );
+    }
+} );
