b4ac7b61b78a0e4ae5179865764b14ec9ec53927
Simon A. Eugster <simon.eu@gmail.com>
2015-12-30 11:25:15 +0100

    Watch directory

--- app.js
+++ app.js
@@ -10,30 +10,42 @@ var nextId = (function () {
 /**
  * @param {string} oraPath
  * @param {string} outDir
+ * @param {boolean=} restarted
  */
-var startWatcher = function ( oraPath, outDir ) {
+var startWatcher = function ( oraPath, outDir, restarted ) {
 
     var watchEnded = false,
         id = nextId();
 
     var outFilename = path.basename( oraPath ).replace( '.ora', '.png' );
 
-    console.log( 'Will save to ', outFilename, ' in ', outDir );
+    console.log( id, 'Will save to ', outFilename, ' in ', outDir );
     var outFile = path.join( outDir, outFilename );
 
-    var toImage = () => {
+    /**
+     * @param {number} retries Number of retries
+     */
+    var toImage = ( retries ) => {
+        console.log( id, 'Converting to PNG ...' );
         try {
             oti.oraToImage( oraPath, outFile, function ( err ) {
-                console.log( 'PNG generated: ', !err );
+                console.log( id, 'PNG generated: ', !err );
+                if ( err ) {
+                    if ( retries > 0 ) {
+                        console.log( id, 'Failed generating ' + outFile + ', trying again ... (' + err + ')' );
+                        setTimeout( () => toImage( retries - 1 ), 100 );
+                    } else {
+                        console.log( id, 'Failed generating ' + outFile );
+                        console.warn( err );
+                    }
+                }
             } );
         } catch ( err ) {
             console.warn( err );
         }
     };
 
-    console.log( 'Starting watcher ', id );
-
-    fs.watch( oraPath, function ( event, filename ) {
+    var watcher = function ( event, filename ) {
         if ( watchEnded ) {
             console.log( id, 'Event received although watch ended: ', event, filename );
             return;
@@ -41,43 +53,64 @@ var startWatcher = function ( oraPath, outDir ) {
 
         if ( event === 'change' ) {
             console.log( id, 'File changed:', filename );
-            toImage();
+            toImage( 1 );
 
         } else if ( event === 'rename' ) {
             console.log( id, 'File was renamed: ', filename );
             watchEnded = true;
-            startWatcher( oraPath, outDir );
+            startWatcher( oraPath, outDir, true );
 
         } else {
             console.warn( id, 'Unknown event: ', event, filename );
         }
 
-    } );
+    };
 
-    try {
-        toImage();
-    } catch ( err ) {
-        console.warn( 'Could not convert: ', err );
-    }
+    var startWatching = function ( restart ) {
+        console.log( 'Starting watcher ', id );
+
+        try {
+
+            fs.watch( oraPath, watcher );
+            if ( !restarted ) {
+                toImage( 1 );
+            }
+
+        } catch ( e ) {
+            if ( restart ) {
+                setTimeout( () => startWatching( false ), 100 );
+            } else {
+                console.warn( 'Could not start watcher for ' + oraPath, e );
+            }
+        }
+    };
+
+    startWatching( true );
 };
 
 function observe( boardDir, outDir ) {
 
     console.log( 'Observing ', boardDir, ' and generating files to ', outDir );
 
+    /** List of watched files. */
     var watched = [];
+
     scan();
 
     function scan() {
+
         fs.readdir( boardDir, function ( err, files ) {
+
             if ( err ) {
                 console.warn( err );
             } else {
+
+                // List files in directory, and watch new ones.
                 files.forEach( ( f ) => {
                     if ( f.endsWith( '.ora' ) ) {
 
                         if ( watched.indexOf( f ) >= 0 ) {
-                            console.log( 'Already watching: ', f );
+                            // Already watching this file.
                         } else {
                             console.log( 'ORA found: ', f );
                             startWatcher( path.join( boardDir, f ), outDir );
@@ -87,8 +120,11 @@ function observe( boardDir, outDir ) {
                         console.log( 'Skipping ', f );
                     }
                 } );
+
             }
         } );
+
+        // Scan again in a bit.
         setTimeout( scan, 4000 );
     }
 }
